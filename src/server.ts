abfc2fc80aebd504be60c3fb892cc69a51f36f5f
Usnaamer <gaspard.munguiaxica2004@gmail.com>
2026-02-11 02:14:54 +0100

    6.1 Exercice 1 : API JSON et Array

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,33 @@ import express, { type Request, type Response } from "express";
 const app = express();
 const port = 3000; // le port 
 
-app.get('/', (req: Request, res: Response) => {
+interface Etudiant { // interface pour définir la structure d'un objet étudiant
+    id: number;
+    nom: string;
+    prenom: string;
+}
+
+const etudiants : Etudiant[] = [
+{ id: 1, nom: "Dupont", prenom: "Jean" },
+{ id: 2, nom: "Martin", prenom: "Sophie" },
+{ id: 3, nom: "Doe", prenom: "John" },
+];
+
+
+app.get('/', (req: Request, res: Response) => { // Route par défaut du serveur (racine  "/")
     res.send('Bienvenue sur mon serveur API');
 });
 
-app.listen(port, () => {
+app.get('/api/data', (req: Request, res: Response) => { // route pour récupérer les données des étudiants c'est la route qui donne les data des API
+    res.json(etudiants); // permet de changer les données en format JSON afin que le typeScript puisse les comprendre et les utiliser facilement
+});
+
+app.listen(port, () => { // callBack dans ma console pour indiquer le status, je devrais faire un try except pour gerer les erreurs inatendues
     console.log('Bienvenue sur mon serveur API')
     console.log(`Serveur lancé !! :  http://localhost:${port}`);
 });
 
+// Exemple de fonction simple pour tester le fonctionnement de TypeScript
 function greet(name:string): string {
     return `Hello, ${name}! de BAC 2`;
 }
