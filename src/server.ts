1f60cd3fcd7fb3e5bc5a665ccfd5e958971011eb
Usnaamer <gaspard.munguiaxica2004@gmail.com>
2026-02-11 02:29:39 +0100

    6.2 Exercice 2 : Paramètres dynamiques

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,8 @@ const etudiants : Etudiant[] = [
 { id: 3, nom: "Doe", prenom: "John" },
 ];
 
+const name = { "message": "Bonjour Yves", "timestamp":"2026-01-29T12:00:19.821Z" } // exemple de données pour tester l'API
+
 
 app.get('/', (req: Request, res: Response) => { // Route par défaut du serveur (racine  "/")
     res.send('Bienvenue sur mon serveur API');
@@ -27,13 +29,21 @@ app.get('/api/data', (req: Request, res: Response) => { // route pour récupére
     res.json(etudiants); // permet de changer les données en format JSON afin que le typeScript puisse les comprendre et les utiliser facilement
 });
 
+app.get('/api/hello/:name', (req: Request, res: Response) => { // route pour dire bonjour à une personne en particulier, le ":name" est un paramètre dynamique qui peut être remplacé par n'importe quel nom
+   const nameParam = req.params.name; // on récupère le nom à partir des paramètres de la requête
+    res.json({ // on construit une réponse JSON avce un message e bienvenue et un timestamp
+        message: `Bonjour ${nameParam}`,
+        timestamp: new Date().toISOString()
+    });
+});
+
 app.listen(port, () => { // callBack dans ma console pour indiquer le status, je devrais faire un try except pour gerer les erreurs inatendues
     console.log('Bienvenue sur mon serveur API')
     console.log(`Serveur lancé !! :  http://localhost:${port}`);
 });
 
 // Exemple de fonction simple pour tester le fonctionnement de TypeScript
-function greet(name:string): string {
-    return `Hello, ${name}! de BAC 2`;
+function greet(nom:string): string {
+    return `Hello, ${nom}! de BAC 2`;
 }
 console.log(greet("Gaspard"));
