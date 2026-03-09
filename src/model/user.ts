27720fda09d5d04e8c5fd5c662615b3953663764
Usnaamer <gaspard.munguiaxica2004@gmail.com>
2026-03-09 09:52:51 +0100

    exteding model

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,23 +1,26 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
-const User = sequelize.define(
-  'User',
+class User extends Model {}
+
+User.init(
   {
     // Model attributes are defined here
-    firstName: {
+    prenom : {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    lastName: {
+    nom : {
       type: DataTypes.STRING,
-      // allowNull defaults to true
+      allowNull: false,
     },
   },
   {
     // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
   },
 );
 
-// `sequelize.define` also returns the model
+// the defined model is the class itself
 console.log(User === sequelize.models.User); // true
